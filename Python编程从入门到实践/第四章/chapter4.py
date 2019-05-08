bd6ebc9c5a42931e0f98f1518fc6afe8d99894a7
choly1985 <434375025@qq.com>
2019-05-08 13:56:56 +0800

    day0508++

---
+++ Python编程从入门到实践/第四章/chapter4.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-05-08 09:02:24
+# @Author  : liuhua (434375025@qq.com)
+# @Link    : https://github.com/choly1985
+# @Version : $Id$
+
+import os
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(magician.title() + ",that was a great trick!")
+#     print("I can't wait to see your next trick," + magician.title() + ".\n")
+
+# numbers = list(range(1, 101, 2))
+# number_ = list(range(100))
+# print(numbers, number_,sep='\n')
+
+# squares = []
+# for value in range(1, 11):
+#     squares.append(value**2)
+# print(squares)
+
+# digists = list(range(10))
+# print(min(digists), max(digists), sum(digists))
+
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+player = ['chrales', 'martina', 'michael', 'florence', 'eli']
+
+# player_copy = player[:]
+# player.append('CR7')
+# print(player_copy) #副本的值给了新列表，新增加元素没有
+
+# player_copy = player
+# player.append('CR7')
+# print(player_copy)  #关联到了列表
+
+# postition = [(0, 5), (1, 2)]
+# postition[0] = (1, 2)
+# print(postition)
+
+# postion = ([1], [3])
+# postion[1].append(2)# 改变tuple内容
+# print(postion)
