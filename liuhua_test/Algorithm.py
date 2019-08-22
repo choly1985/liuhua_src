1f0896982c9cc19ca03c84ff71a7270b5931a1a2
choly1985 <434375025@qq.com>
2019-08-22 14:23:33 +0800

    123

---
+++ liuhua_test/Algorithm.py
@@ -0,0 +1,22 @@
+'''
+@Description: python
+@Version: 1.0
+@Autor: liuhua
+@QQ: 434375025@qq.com
+@Link: https://github.com/choly1985
+@Date: 2019-08-22 12:50:47
+@LastEditors: liuhua
+'''
+
+
+def sort():
+    '''
+    @description: python
+    @param {type} param
+    @return: None
+    @author: liuhua
+    '''
+    print(123)
+
+
+sort()
