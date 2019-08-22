b842bd91ad109c4b473228bde4126fcc15181a6d
choly1985 <434375025@qq.com>
2019-08-22 14:27:24 +0800

    data test

---
+++ liuhua_test/dataStructure.py
@@ -0,0 +1,16 @@
+'''
+@Description: python
+@Version: 1.0
+@Autor: liuhua
+@QQ: 434375025@qq.com
+@Link: https://github.com/choly1985
+@Date: 2019-08-22 14:25:24
+@LastEditors: liuhua
+'''
+
+
+def prrint_log():
+    print(123)
+
+
+prrint_log()
